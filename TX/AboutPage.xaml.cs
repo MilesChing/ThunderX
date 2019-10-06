05f138583d6570f11e06d82022982d843cbf6d17
Miles Ching <qinzhengrui@live.cn>
2019-10-06 20:42:20 +0800

    修正界面的显示错误

--- TX/AboutPage.xaml.cs
+++ TX/AboutPage.xaml.cs
@@ -38,7 +38,7 @@ namespace TX
             if (license == null) return;
             if (license.IsActive)
             {
-                if (true)
+                if (license.IsTrial)
                 {
                     TrialPanel.Visibility = Visibility.Visible;
                     ActivePanel.Visibility = Visibility.Collapsed;
