3f83b565d26dda92ca49d1233bac22fd1076e97e
Qin Zhengrui <qinzhengrui@live.cn>
2021-01-24 22:26:24 +0800

    add test framework for http downloader

---
+++ UnitTestProject/Utils/FakeFolderProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TX.Core.Interfaces;
+using Windows.Storage;
+
+namespace TXUnitTest.Utils
+{
+    /// <summary>
+    /// FakeFolderProvider always provides LocalCacheFolder for test.
+    /// </summary>
+    class FakeFolderProvider : IFolderProvider
+    {
+        public Task<IStorageFolder> GetFolderFromTokenAsync(string token) =>
+            Task.Run<IStorageFolder>(() => ApplicationData.Current.LocalCacheFolder);
+    }
+}
