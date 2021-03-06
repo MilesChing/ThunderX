086ec93bac248787e475f18147e06cc851da7df5
Qin Zhengrui <qinzhengrui@live.cn>
2021-03-06 13:10:39 +0800

    refactor background task

---
+++ TX/Background/AbstractBackgroundTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace TX.Background
+{
+    /// <summary>
+    /// AbstractBackgroundTask is an abstract of background
+    /// tasks managed by BackgroundTaskManager.
+    /// </summary>
+    abstract class AbstractBackgroundTask
+    {
+        /// <summary>
+        /// Name of the background task.
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// Run background task with given instance.
+        /// </summary>
+        /// <param name="taskInstance">Background task instance.</param>
+        public abstract void Run(IBackgroundTaskInstance taskInstance);
+
+        /// <summary>
+        /// Get a builder which build the background task.
+        /// This abstract method is used for successors to append
+        /// their triggers and conditions before registered.
+        /// </summary>
+        /// <returns>Builder which build current background task.</returns>
+        public abstract BackgroundTaskBuilder Build();
+    }
+}
