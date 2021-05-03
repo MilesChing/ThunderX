5bcc8b3831f33cad5ace82674e46fa951398e941
Qin Zhengrui <qinzhengrui@live.cn>
2021-05-03 09:52:54 +0800

    add collection binds

---
+++ TX/Collections/CollectionBind.cs
@@ -0,0 +1,145 @@
+﻿using EnsureThat;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TX.Collections
+{
+    /// <summary>
+    /// CollectiondBind bind modifications or changes from one collection 
+    /// to another. It resyncs and matches each object between both collections
+    /// once enabled, to ensure that collection source and destination are same.
+    /// Then it listens to each collection change of source and apply the same
+    /// change to destination.
+    /// </summary>
+    /// <typeparam name="S">The type of items in the source collection.</typeparam>
+    /// <typeparam name="T">The type of items in the destination collection.</typeparam>
+    public class CollectionBind<S, T>
+    {
+        /// <summary>
+        /// Construct a CollectionBind from a source collection which 
+        /// should be an ISyncableCollection to a destination collection.
+        /// It uses the caster when new object is required to be created
+        /// and uses the comparer to match existence objects in two 
+        /// collections when resyncing.
+        /// </summary>
+        /// <param name="source">The source collection.</param>
+        /// <param name="destination">The destination collection.</param>
+        /// <param name="caster">The caster creates an object of type 
+        /// <typeparamref name="T"/> from an object of type <typeparamref name="S"/>.
+        /// </param>
+        /// <param name="comparer">The comparer compares an object of type 
+        /// <typeparamref name="T"/> with an object of type <typeparamref name="S"/> 
+        /// and returns true if they are corresponding.</param>
+        public CollectionBind(
+            ISyncableEnumerable<S> source, 
+            ICollection<T> destination,
+            Func<S, T> caster,
+            Func<S, T, bool> comparer)
+        {
+            Ensure.That(source, nameof(source)).IsNotNull();
+            Ensure.That(destination, nameof(destination)).IsNotNull();
+            Ensure.That(caster, nameof(caster)).IsNotNull();
+            Ensure.That(comparer, nameof(comparer)).IsNotNull();
+            Source = source;
+            Destination = destination;
+            Caster = caster;
+            Comparer = comparer;
+        }
+
+        /// <summary>
+        /// Controls whether the bind is enabled.
+        /// </summary>
+        public bool IsEnabled
+        {
+            get => _is_enbaled;
+            set
+            {
+                if (value != _is_enbaled)
+                {
+                    _is_enbaled = value;
+                    if (_is_enbaled)
+                    {
+                        Source.CollectionChanged += SourceCollectionChanged;
+                        Resync();
+                    }
+                    else Source.CollectionChanged -= SourceCollectionChanged;
+                }
+            }
+        }
+        private bool _is_enbaled = false;
+
+        /// <summary>
+        /// The source collection.
+        /// </summary>
+        public ISyncableEnumerable<S> Source { get; private set; }
+
+        /// <summary>
+        /// The destination collection.
+        /// </summary>
+        public ICollection<T> Destination { get; private set; }
+
+        /// <summary>
+        /// The caster creates an object of type <typeparamref name="T"/> 
+        /// from an object of type <typeparamref name="S"/>.
+        /// </summary>
+        public Func<S, T> Caster { get; private set; }
+
+        /// <summary>
+        /// The comparer compares an object of type <typeparamref name="T"/> 
+        /// with an object of type <typeparamref name="S"/> 
+        /// and returns true if they are corresponding.
+        /// </summary>
+        public Func<S, T, bool> Comparer { get; private set; }
+    
+        private void Resync()
+        {
+            List<T> toBeDeleted = new List<T>();
+            List<S> toBeCreated = new List<S>();
+            foreach (var s in Source)
+            {
+                int createdCount = Math.Max(0,
+                    Destination.Where(t => Comparer(s, t)).Count() -
+                    Source.Count(item => item.Equals(s)));
+                for (int i = 0; i < createdCount; ++i)
+                    toBeCreated.Add(s);
+            }
+
+            foreach (var t in Destination)
+                if (!Source.Any(s => Comparer(s, t)))
+                    toBeDeleted.Add(t);
+
+            foreach (var deleted in toBeDeleted)
+                Destination.Remove(deleted);
+            foreach (var created in toBeCreated)
+                Destination.Add(Caster(created));
+        }
+
+        private void SourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Reset:
+                    Destination.Clear();
+                    break;
+                case NotifyCollectionChangedAction.Add:
+                case NotifyCollectionChangedAction.Remove:
+                case NotifyCollectionChangedAction.Replace:
+                    if (e.OldItems != null)
+                        foreach (S toBeRemovedS in e.OldItems)
+                        {
+                            var toBeRemovedD = Destination.First(
+                                t => Comparer(toBeRemovedS, t));
+                            Destination.Remove(toBeRemovedD);
+                        }
+                    if (e.NewItems != null)
+                        foreach (S toBeCreatedS in e.NewItems)
+                            Destination.Add(Caster(toBeCreatedS));
+                    break;
+            }
+        }
+    }
+}
---
+++ UnitTestProject/CollectionsTest/CollectionAndWrapperTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TX.Collections;
+
+namespace UnitTestProject.CollectionsTest
+{
+    class TestObservableCollection<T> : 
+        ObservableCollection<T>, 
+        ISyncableEnumerable<T> { }
+
+    [TestClass]
+    public class CollectionAndWrapperTest
+    {
+        [TestMethod]
+        public void TestBaseOperations()
+        {
+            var testSrc1 = new TestObservableCollection<int>();
+            var testDest1 = new Collection<int>();
+            var testSrc2 = new TestObservableCollection<int>();
+            var testDest2 = new Collection<int>();
+            Func<int, int> caster = src => src;
+            Func<int, int, bool> comparer = (src, dest) => dest == caster(src);
+
+            var bind1 = new CollectionBind<int, int>(
+                testSrc1, testDest1, caster, comparer);
+            var bind2 = new CollectionBind<int, int>(
+                testSrc2, testDest2, caster, comparer);
+            bind1.IsEnabled = true;
+            bind2.IsEnabled = true;
+
+            for (int batch = 0; batch < 50; ++batch)
+            {
+                bind1.IsEnabled = bind2.IsEnabled = (batch % 4 != 0);
+
+                for (int i = 0; i < 100; ++i)
+                    TestAdd(testSrc1, testSrc2);
+
+                for (int i = 0; i < 50; ++i)
+                    TestRemove(testSrc1, testSrc2);
+
+                TestClear(testSrc1, testSrc2);
+
+                bind1.IsEnabled = bind2.IsEnabled = true;
+                Assert.IsTrue(CompareCollection(testSrc1, testDest1));
+                Assert.IsTrue(CompareCollection(testSrc2, testDest2));
+                Assert.IsTrue(CompareCollection(testDest1, testDest2));
+                Assert.AreEqual(testSrc1.Count, testDest1.Count);
+                Assert.AreEqual(testSrc2.Count, testDest2.Count);
+                Assert.AreEqual(testDest1.Count, testDest2.Count);
+            }
+        }
+
+        private bool CompareCollection(ICollection<int> col1, ICollection<int> col2)
+            => !(col1.Except(col2).Any() || col2.Except(col1).Any());
+
+        private void TestAdd(params ICollection<int>[] srcs)
+        {
+            int newVal = randomEngine.Next() % 1024;
+            foreach (var src in srcs) src.Add(newVal);
+        }
+
+        private void TestRemove(params ICollection<int>[] srcs)
+        {
+            int removedVal = srcs[0].FirstOrDefault();
+            foreach (var src in srcs) src.Remove(removedVal);
+        }
+
+        private void TestClear(params ICollection<int>[] srcs)
+        {
+            foreach (var src in srcs) src.Clear();
+        }
+
+        Random randomEngine = new Random();
+    }
+}
